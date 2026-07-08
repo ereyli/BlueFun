f3bf4485f6d56a0fd5183ca01a18bccb21181d4b
ereyli <ereyli_95@hotmail.com>
2026-07-08 13:53:03 +0300

    Customize wallet button states for connect and network

--- apps/web/src/components/wallet-button.tsx
+++ apps/web/src/components/wallet-button.tsx
@@ -3,5 +3,34 @@
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 
 export function WalletButton() {
-  return <ConnectButton showBalance={false} chainStatus="icon" accountStatus="address" />;
+  return (
+    <ConnectButton.Custom>
+      {({ account, chain, mounted, openAccountModal, openChainModal, openConnectModal }) => {
+        const ready = mounted;
+        const connected = ready && account && chain;
+
+        if (!connected) {
+          return (
+            <button className="button primary" disabled={!ready} onClick={openConnectModal} type="button">
+              Connect Wallet
+            </button>
+          );
+        }
+
+        if (chain.unsupported) {
+          return (
+            <button className="button primary" onClick={openChainModal} type="button">
+              Wrong Network
+            </button>
+          );
+        }
+
+        return (
+          <button className="button" onClick={openAccountModal} type="button">
+            {account.displayName}
+          </button>
+        );
+      }}
+    </ConnectButton.Custom>
+  );
 }
